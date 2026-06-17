de98311f43834d7ba857b21b3284563dfd017c9b
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-06-18 02:45:50 +0600

    Update ACCESS_TOKEN_EXPIRES_IN to 7 days and add SUPER_ADMIN_EMAIL constant

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,5 +1,6 @@
-export const ACCESS_TOKEN_EXPIRES_IN = "15m";
+export const ACCESS_TOKEN_EXPIRES_IN = "7d";
 export const REFRESH_TOKEN_EXPIRES_IN = "30d";
 export const OTP_EXPIRES_MINUTES = 10;
 export const PASSWORD_SALT_ROUNDS = 10;
 export const OTP_LENGTH = 6;
+export const SUPER_ADMIN_EMAIL = "mdjuyelrana.com.bd1@gmail.com";
