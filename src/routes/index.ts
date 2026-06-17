35cfe37e466143e4780330053628414fe8190b1d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-06-18 02:51:00 +0600

    Add user and product routes to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.route";
+import userRoutes from "../modules/users/users.route";
+import productRoutes from "../modules/products/products.route";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/products", productRoutes);
 
 export default router;
