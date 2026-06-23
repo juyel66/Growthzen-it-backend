fc539b3614d8eaf543ba5d4d742da181eccc47d6
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-06-23 16:00:55 +0600

    Add routes for settings, orders, and dashboard management

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.route";
+import dashboardRoutes from "../modules/dashboardManagement/dashboard.route";
+import orderRoutes from "../modules/orders/orders.route";
+import settingsRoutes from "../modules/settings/settings.route";
 import userRoutes from "../modules/users/users.route";
 import productRoutes from "../modules/products/products.route";
 
@@ -8,5 +11,8 @@ const router = Router();
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
+router.use("/settings", settingsRoutes);
+router.use("/orders", orderRoutes);
+router.use("/dashboard-management", dashboardRoutes);
 
 export default router;
