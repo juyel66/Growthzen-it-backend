e88d3da8d076e9850d3328a95db205ce034104ca
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-06-26 23:05:38 +0600

    remove commented import in prisma.ts

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
+// import {PrismaPrimaryClient} from "../../../prisma" 
+
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
